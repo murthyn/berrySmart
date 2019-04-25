475196f066cf87d3d09c96e5fb30139e053e5617
Nikhil Murthy <nmurthy@mit.edu>
2019-04-25 19:45:40 -0400

    sensor updates

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -1,9 +1,15 @@
 #include <SPI.h>
 #include <TFT_eSPI.h>
+#include <dht.h>
 
+dht DHT;
 TFT_eSPI tft = TFT_eSPI();
 
+#define DHT11_PIN 19
+
 uint32_t counter; //used for timing
+
+// https://github.com/RobTillaart/Arduino/tree/master/libraries/DHTstable
 
 void setup() {
   Serial.begin(115200); // Set up serial port
@@ -14,18 +20,33 @@ void setup() {
   tft.fillScreen(TFT_WHITE); //fill background
   tft.setTextColor(TFT_BLACK, TFT_WHITE); //set color for font
 
+  //pinMode(DHT11_PIN, INPUT_PULLUP);
+  
   counter = millis();
 
 }
 
 void loop() {
-  if (millis() - counter > 1000) {
+  if (millis() - counter > 3000) {
     tft.setCursor(0, 0, 1); //set cursor, font size 1
-    char message[20]; //char buffer allocate
+    
+    char message[100]; //char buffer allocate
     sprintf(message, "Brightness: %2.4f     ", 1 - analogRead(A7) / 4096.0); //message about brightness
     tft.println(message); //print
     Serial.println(message);
+
+    tft.setCursor(0, 50, 1); //set cursor, font size 1
+
+    int chk = DHT.read11(DHT11_PIN);
+    Serial.print("Temperature = ");
+    Serial.println(DHT.temperature);
+    Serial.print("Humidity = ");
+    Serial.println(DHT.humidity);
+
+    char message2[100]; //char buffer allocate
+    sprintf(message2, "Temp/humidity: %2.4f %2.4f", DHT.temperature, DHT.humidity); //message about brightness
+    tft.println(message2); //print
+    
     counter = millis();
   }
-  //backlight.update(); //callupdate as fast as possible on our software PWM object
 }
