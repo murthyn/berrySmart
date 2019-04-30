44c422fb7a3b000b9a1280e7a8e5da512b0c8c4a
Gabriella Garcia <gabriella@LATITUDE.mit.edu>
2019-04-30 19:20:55 -0400

    Blueberry hadler stripped down

--- server/blueberry_Handler.py
+++ server/blueberry_Handler.py
@@ -3,32 +3,34 @@ import random
 import string
 import datetime
 
-
+	
 blueberry_db = '__HOME__/blueberry.db'
 
 
 def request_handler(request):
-     # SQL
-    conn = sqlite3.connect(blueberry_db)  # connect to that database (will create if it doesn't already exist)
-    c = conn.cursor()  # make cursor into database (allows us to execute commands)
-    c.execute('''CREATE TABLE IF NOT EXISTS blueberry_table (word str, timing timestamp);''') # run a CREATE TABLE command
+	 # SQL
+	conn = sqlite3.connect(blueberry_db)  # connect to that database (will create if it doesn't already exist)
+	c = conn.cursor()  # make cursor into database (allows us to execute commands)
+	c.execute('''CREATE TABLE IF NOT EXISTS blueberry_table (word str, timing timestamp);''') # run a CREATE TABLE command
 
-    if request['method'] == 'GET':
-        allthings = c.execute('''SELECT  word,timing FROM blueberry_table ORDER BY timing DESC;''').fetchall()
-        things = None
+	if request['method'] == 'GET':
+		allthings = c.execute('''SELECT  word,timing FROM blueberry_table ORDER BY timing DESC;''').fetchall()
+		things = []
 
-        if len(allthings) >= 1:
-          return allthings
+		if len(allthings) >= 1:
+			for data in allthings:
+				things.append(data[0])
+			return things
 
-        else:
-            return ''' Not enough values are there in the database'''
-            
+		else:
+			return ''' Not enough values are there in the database'''
+			
 
-    # POST:
-    else:
-        text = request['form']['text']
-                       
-        c.execute('''INSERT into blueberry_table VALUES (?,?);''', (text,datetime.datetime.now()))
-        
-    conn.commit() # commit commands
-    conn.close() # close connection to database
+	# POST:
+	else:
+		text = request['form']['text']
+					   
+		c.execute('''INSERT into blueberry_table VALUES (?,?);''', (text,datetime.datetime.now()))
+		
+	conn.commit() # commit commands
+	conn.close() # close connection to database
