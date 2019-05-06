31fd19bf69211d01c03d272ef35a4703db8cbf32
Gabriella Garcia <gabriella@LATITUDE.mit.edu>
2019-05-05 21:59:35 -0400

    Fixed disaster

--- server/Handlers/strawberryHandler.py
+++ server/Handlers/strawberryHandler.py
@@ -3,22 +3,24 @@ import random
 import string
 import datetime
 
-
-berry3_db = '__HOME__/berry3.db'
+    
+strawberrydb = '__HOME__/blueberry.db'
 
 
 def request_handler(request):
      # SQL
-    conn = sqlite3.connect(berry3_db)  # connect to that database (will create if it doesn't already exist)
+    conn = sqlite3.connect(strawberrydb)  # connect to that database (will create if it doesn't already exist)
     c = conn.cursor()  # make cursor into database (allows us to execute commands)
-    c.execute('''CREATE TABLE IF NOT EXISTS berry3_table (lat float, lon float, temp float, humid float, light float, moist float, timing timestamp);''') # run a CREATE TABLE command
+    c.execute('''CREATE TABLE IF NOT EXISTS strawberrytable (word str, timing timestamp);''') # run a CREATE TABLE command
 
     if request['method'] == 'GET':
-        allthings = c.execute('''SELECT  lat,lon,temp,humid,light,moist,timing FROM berry3_table ORDER BY timing DESC;''').fetchall()
-        things = None
+        allthings = c.execute('''SELECT  word,timing FROM strawberrytable ORDER BY timing DESC;''').fetchall()
+        things = []
 
         if len(allthings) >= 1:
-          return allthings
+            for data in allthings:
+                things.append(data[0])
+            return things
 
         else:
             return ''' Not enough values are there in the database'''
@@ -26,14 +28,9 @@ def request_handler(request):
 
     # POST:
     else:
-        lat = request['form']['lat']
-        lon =request['form']['lon']
-        temp = request['form']['temp']
-        humid = request['form']['humid']
-        light = request['form']['light']
-        moist = request['form']['moist']
-               
-        c.execute('''INSERT into berry3_table VALUES (?,?,?,?,?,?,?);''', (lat,lon,temp,humid,light,moist,datetime.datetime.now()))
+        text = request['form']['text']
+                       
+        c.execute('''INSERT into strawberrytable VALUES (?,?);''', (text,datetime.datetime.now()))
         
     conn.commit() # commit commands
     conn.close() # close connection to database
