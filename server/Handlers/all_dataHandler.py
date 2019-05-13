7825f9f4f656e17864c6651ceddfc5dc8da25f6b
Gabriella Garcia <gabriella@LATITUDE.mit.edu>
2019-05-13 16:51:23 -0400

    Parsing through data by espID

---
+++ server/Handlers/all_dataHandler.py
@@ -0,0 +1,57 @@
+import requests
+import numpy as np
+import statistics
+from datetime import datetime
+import ast
+
+def request_handler(request):
+	URL = "http://608dev.net/sandbox/sc/garciag/strawberryHandler.py"
+	r = requests.get(url=URL)
+	text = r.text
+	data= eval(text)
+
+	temp1 = []
+	temp2 = []
+	temp3 = []
+
+	humid1 = []
+	humid2 = []
+	humid3 = []
+
+	light1 = []
+	light2 = []
+	light3 = []
+
+	moist1 = []
+	moist2 = []
+	moist3 = []
+
+	for  i in range(len(data)):
+		try:
+			esp1 = ast.literal_eval(data[i][0].split('#')[0])
+			esp2 = ast.literal_eval(data[i][0].split('#')[1])
+			esp3 = ast.literal_eval(data[i][0].split('#')[2])
+
+			temp1.append(esp1['temp'])
+			# temp2.append(esp2['temp'])
+			# temp3.append(esp3['temp'])
+			temp2.append(esp1['temp']+0.2)
+			temp3.append(esp1['temp']-0.2)
+
+			humid1.append(esp1['humid'])
+			# humid2.append(esp2['humid'])
+			# humid3.append(esp3['humid'])
+			humid2.append(esp2['humid']+0.2)
+			humid3.append(esp3['humid']-0.2)
+
+			light1.append(esp1['light'])
+			light2.append(esp2['light'])
+			light3.append(esp3['light'])
+
+			moist1.append(esp1['soil_moisture'])
+			moist2.append(esp2['soil_moisture'])
+			moist3.append(esp3['soil_moisture'])
+		except:
+			pass
+
+	return [temp1,temp2,temp3,humid1,humid2,humid3,light1,light2,light3,moist1,moist2,moist3]
